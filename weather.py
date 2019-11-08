23bfcc7a152b7d9d184476874195a27fc93aff72
jonay-ve <57045699+jonay-ve@users.noreply.github.com>
2019-11-08 22:26:48 +0000

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,9 +1,7 @@
-# -*- coding: utf-8 -*-
-
 import http.client
 import ssl
 import json
-from datetime import date, timedelta
+import datetime
 
 #Usamos la API de la AEMET que nos permite ver la predicción del tiempo en el día en curso hasta un horizonte de 7 días, 
 #tomando como parámetro de entrada el municipio. Los datos se actualizan continuamente.
@@ -12,10 +10,10 @@ from datetime import date, timedelta
 file_aemet = open("aemet_api_key.txt")
 apyKey = file_aemet.readline()
 file_aemet.close()
-
-#Alternativa, pasar la APIKEY por parametro, comentar las líneas anteriores y desconmentar la siguiente
 """
-apyKey='pegar la clave'
+#Alternativa, pasar la APIKEY por parametro, comentar las líneas anteriores y desconmentar la siguiente
+
+apyKey='PEGAR LA CLAVE'
 """
 
 #Código para usar la API de la AEMET
@@ -36,39 +34,72 @@ def getMunPrediction(codigoMun):
     res= conn.getresponse()
     datos = res.read().decode('utf-8','ignore')
     datos= json.loads(datos)
-    return(datos)
+    return datos
 
-#Comprobación si existen predicciones disponibles
-def fechaRangoPrediccion():
-    #Evalua si una fecha de un evento está dentro del rango de predicción
-    #que tiene disponible la AEMET
-    #encontré la siguiente pagina para hacer el caso
-    #https://python-para-impacientes.blogspot.com/2014/02/operaciones-con-fechas-y-horas.html
-    hoy=date.today()
-    diasMax=hoy + timedelta(days=6)
+#Pasa un objeto tipo str en formato dd-mm-aaaa a un tipo de dato datetime
+def srtDatetime(fechaStr):
+    fechaStr = datetime.datetime.strptime(fechaStr, '%d-%m-%Y')
+    return fechaStr
+
+#Comprobación si existen predicciones disponibles: evalua si una fecha
+#de un evento está dentro del rango de predicción que tiene disponible la AEMET
+#Se usa como guía la web:
+#https://python-para-impacientes.blogspot.com/2014/02/operaciones-con-fechas-y-horas.html    
+def fechaRangoPrediccion(fechaEvento):
+
+    #Alcance máximo de predicción ofrecida por la AEMET
+    diasMax=datetime.datetime.today() + datetime.timedelta(days=6)
+    
+    #Paso de tipo de datos str a datetime
+    fechaEvento = srtDatetime(fechaEvento)
+    
     #calcula la diferencia de días
-    resta=diasMax-hoy
+    resta=diasMax-fechaEvento
+    
     #comprueba que el día que se pide es está dentro del alcance de la
     #predicción proporcionada por AEMET
     if resta.days<7 and resta.days>-1:
         contenido='TRUE'
     else:
         contenido='FALSE'
+    
     return contenido
 
 #Datos que queremos presentar/guardar en nuestro repositorio  
-def getWeather(codigoMun):
-    #Obtener la tempetara máxima y mínima para el día de la consulta
-    if fechaRangoPrediccion()=='FALSE':
-        print("Sin datos disponibles de predicción")
+def getWeather(codigoMun,fecha):
+    
+    #Comprobamos si la fecha es válida
+    if fechaRangoPrediccion(fecha)=='FALSE':
+        datosWeather="Sin datos disponibles de predicción"
     else:
+        #se calcula el día objetivo para obtener los datos en forma de índice
+        fecha=srtDatetime(fecha)
+        hoy=datetime.datetime.today()
+        diferencia= fecha - hoy
+        resta=diferencia.days+1
+        
+        #Obtenemos la predicción del municipio de interés
         dat=getMunPrediction(codigoMun)
-        print('Temperatura máxima: ', dat[0]['prediccion']['dia'][0]['temperatura']['maxima'])
-        print('Temperatura mínima: ', dat[0]['prediccion']['dia'][0]['temperatura']['minima'])
-    return
-#vista previa para el municipio de la Laguna
+        
+        #Se guardan los datos de la predicción en una lista
+        datosWeather={'estadoCielo': dat[0]['prediccion']['dia'][resta]['estadoCielo'][0]['descripcion'],
+                      'probPrecipitacion':dat[0]['prediccion']['dia'][resta]['probPrecipitacion'][0]['value'],
+                      'sensTermMax':dat[0]['prediccion']['dia'][resta]['sensTermica']['maxima'],
+                      'sensTermMin':dat[0]['prediccion']['dia'][resta]['sensTermica']['minima'],
+                      'temperaturaMax':dat[0]['prediccion']['dia'][resta]['temperatura']['maxima'],
+                      'temperaturaMin':dat[0]['prediccion']['dia'][resta]['temperatura']['minima']}
+
+    return datosWeather
+
+"""Ejemplo para comprobar código
 municipio='38023'
-getWeather(municipio)
+datossss=getMunPrediction(municipio)
+#vista previa para el municipio de la Laguna
+
+municipio='38024'
+fechaEvento='14-11-2019'
+print(getWeather(municipio,fechaEvento))
+"""
 # municipio Nombre oficial del municipio del evento.
 # fecha Objeto tipo date. Fecha del evento.
 # previsión Lista con valores para estado del cielo, lluvia, temperatura...
