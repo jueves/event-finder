5772e9901c187eb175570d7930c344a2ec2827eb
Luis Cobiella Hernández <alu0100358797@ull.edu.es>
2019-11-09 14:56:03 +0000

    Genera códigos aleatorios. Solución temporal mientras se solucionan bugs.

--- location.py
+++ location.py
@@ -2,21 +2,29 @@
 
 import googlemaps
 import csv
-import requests
+import random
+#import requests
 
 def getPostalCode(location_name):
     # Recibe un objeto string con el nombre de un lugar de Canarias.
     # Devuelve un objeto int con el código postal de dicho lugar.
-    location_name += ", Islas Canarias"
-    
-    # Utiliza la API de GoogleMaps para obtener información sobre el lugar.
-    geocode_result = gmaps.geocode(location_name)
-    
     postal_code = 0
-    for level in geocode_result[0]['address_components']:
-        if (level['types']==['postal_code']):
-            postal_code = int(level['long_name'])
-            break
+    
+    # Comprueba si ya tenemos almacenado el código postal del lugar.
+    if location_name in saved_locations.keys():
+        postal_code = saved_locations[location_name]
+    else:
+        print("saltado")
+        # Utiliza la API de GoogleMaps para obtener información sobre el lugar.
+        geocode_result = gmaps.geocode(location_name + ", Islas Canarias")
+
+        for level in geocode_result[0]['address_components']:
+            if (level['types']==['postal_code']):
+                postal_code = int(level['long_name'])
+                # Añade el código postal nuestro diccionario.
+                saved_locations[location_name] = postal_code
+                break
+    
     return(postal_code)
 
 def getMunicipalityCode(location_name):
@@ -24,8 +32,17 @@ def getMunicipalityCode(location_name):
     # Devuelve un objeto int con el código que AEMET da al municipio
     # del evento.
     # Si no encuentra la localización correctamente devuelve el valor 0.
-    postal_code = getPostalCode(location_name)
-    location_code = codes_dic[postal_code]
+#    location_code = 0
+#    postal_code = getPostalCode(location_name)
+    
+    # Solución temporal mientras se solucionan bugs:
+    # Elige un código postal aleatoriamente.
+    postal_code = random.choice(list(codes_dic.keys())) 
+    
+    if postal_code in codes_dic.keys():
+        location_code = codes_dic[postal_code]
+    
+    
     return(location_code)
     
 
@@ -34,11 +51,13 @@ def getCodesDictionary():
     # municipio según la clasificación del INE y AEMET.
     # Utiliza como fuente de datos una tabla del proyecto de GitHub
     # ds-codigos-postales-ine-es de Íñigo Flores y Pablo Castellano.
-    codes_file_url = "https://github.com/inigoflores/ds-codigos-postales-ine-es/raw/master/data/codigos_postales_municipios.csv"
-    with requests.Session() as s:
-        codes_raw = s.get(codes_file_url)
-        codes_decoded = codes_raw.content.decode('utf-8')
-        codes_csv = csv.reader(codes_decoded.splitlines())
+#    codes_file_url = "https://github.com/inigoflores/ds-codigos-postales-ine-es/raw/master/data/codigos_postales_municipios.csv"
+#    with requests.Session() as s:
+#        codes_raw = s.get(codes_file_url)
+#        codes_decoded = codes_raw.content.decode('utf-8')
+#        codes_csv = csv.reader(codes_decoded.splitlines())
+    with open("codigos_postales_municipios.csv") as file:
+        codes_csv = csv.reader(file)
         codes_dic = {}
         next(codes_csv, None) # Skips header
         for line in codes_csv:
@@ -46,7 +65,7 @@ def getCodesDictionary():
         
         # Add code 0 to manage erros getting location.
         codes_dic[0] = 0
-        return(codes_dic)
+    return(codes_dic)
 
 # Crea el objeto gmaps para trabajar con la API de Google Maps
 file_gmaps = open("gmaps_api_key.txt")
@@ -56,3 +75,7 @@ gmaps = googlemaps.Client(key=gmaps_key)
 
 # Crea el diccionario de códigos postales y códigos de municipios.
 codes_dic = getCodesDictionary()
+
+# Crea un diccionario con los códigos de barras de cada localización para
+# reducir las peticiones a la API de Google Maps.
+saved_locations = {}
